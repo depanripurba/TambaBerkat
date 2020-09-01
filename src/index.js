5d8260f7c4fea6696809e523b481455e066bc52b
Depanri Purba <depanri.purba@gmail.com>
2020-09-02 04:48:40 +0700

    mengurangi react helmet dan react snap

--- src/index.js
+++ src/index.js
@@ -4,10 +4,4 @@ import App from './App';
 import {Provider} from 'react-redux'
 import {store} from './Config/Redux'
 
-const rootElement = document.getElementById("root");
-
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(<Provider store={store}><App /></Provider>, rootElement);
-} else {
-  ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
-}
+ReactDOM.render(<Provider store={store}><App /></Provider>,document.querySelector("#root")) 
